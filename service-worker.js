5bb1da7108651af3e5c3a69f804fe8f6b0b36448
aqsagaza20 <aqsa.gaza20@gmail.com>
2026-03-14 02:28:52 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "medterm-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+
+  "/database/chapter1.json",
+  "/database/chapter2.json",
+  "/database/chapter3.json",
+  "/database/chapter4.json",
+  "/database/chapter5.json",
+  "/database/chapter6.json",
+  "/database/chapter7.json",
+  "/database/chapter8.json",
+  "/database/chapter9.json",
+  "/database/chapter10.json",
+  "/database/chapter11.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
